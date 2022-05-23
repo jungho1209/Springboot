24fa7b5d22a3667fa841e7ac12f9e5a845f36742
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-05-23 23:12:14 +0900

    🎄회원 조회 기능 추가

--- src/main/java/com/example/springboot/User/controller/UserController.java
+++ src/main/java/com/example/springboot/User/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.example.springboot.User.controller;
 
+import com.example.springboot.User.dto.UserDto;
 import com.example.springboot.User.dto.request.UserRequest;
 import com.example.springboot.User.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,9 +18,14 @@ public class UserController {
 
     private final UserService userService; // UserService 클래스를 전달해주는 매개변수 userService
 
+    @GetMapping("/signup")
+    public List<UserDto> searchAllDesc() {
+        return userService.searchAllDesc();
+    }
 
     @PostMapping("/signup")
     public void signUp(@RequestBody @Valid UserRequest userRequest) {
         userService.signUp(userRequest);
     }
+
 }
