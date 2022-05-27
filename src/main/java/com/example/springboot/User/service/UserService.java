bdfc9b45a8244186a9d5ce213404dbd218c74510
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-05-27 18:51:54 +0900

    ⚙ put api 구현

--- src/main/java/com/example/springboot/User/service/UserService.java
+++ src/main/java/com/example/springboot/User/service/UserService.java
@@ -2,9 +2,10 @@ package com.example.springboot.User.service;
 
 import com.example.springboot.User.domain.UserEntity;
 import com.example.springboot.User.domain.repository.UserRepository;
+import com.example.springboot.User.dto.request.PutRequest;
 import com.example.springboot.User.dto.request.UserRequest;
-import com.example.springboot.User.dto.response.UserDto;
-import com.example.springboot.User.dto.response.UserListDto;
+import com.example.springboot.User.dto.response.UserListResponse;
+import com.example.springboot.User.dto.response.UserListResponse.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -36,21 +37,37 @@ public class UserService {
                 .build();
         userRepository.save(userEntity);
     }
+
     @Transactional
-    public void delete(Long id){
+    public void delete(Long id) {
         UserEntity userEntity = userRepository.findById(id)
-                .orElseThrow(() -> new
-                        IllegalArgumentException("해당 계정이 없습니다. id = + id"));
+                .orElseThrow(() -> new RuntimeException("해당 계정이 없습니다. id = + id"));
         userRepository.delete(userEntity);
     }
 
-    @Transactional(readOnly = true)
-    public UserListDto searchAllDesc(){
 
-        List<UserDto> userDtoList = userRepository.findAllByOrderByIdDesc()
+    @Transactional(readOnly = true)
+    public UserListResponse searchAllDesc() {
+
+        List<UserResponse> userList = userRepository.findAllByOrderByIdDesc()
                 .stream()
-                .map(UserDto::new)
-                .collect(Collectors.toList());
-        return new UserListDto(userDtoList);
+                .map(userEntity -> UserResponse.builder()
+                        .accountId(userEntity.getAccountId())
+                        .email(userEntity.getEmail())
+                        .name(userEntity.getName())
+                        .studentId(userEntity.getStudentId())
+                        .sex(userEntity.getSex())
+                        .build()).collect(Collectors.toList());
+
+        return new UserListResponse(userList);
     }
+
+    @Transactional
+    public Long update(Long id, PutRequest putRequest){
+        UserEntity userEntity = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 계정이 없습니다. id = + id"));
+        userEntity.update(putRequest.getAccountId(), putRequest.getPassword(), putRequest.getName());
+        return id;
+    }
+
 }
