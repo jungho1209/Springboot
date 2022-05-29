03fca1e42a431a8d045829105a240b2a886492b6
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-05-29 22:21:00 +0900

    🌻 Entity 단 오류 수정

--- src/main/java/com/example/springboot/User/domain/UserEntity.java
+++ src/main/java/com/example/springboot/User/domain/UserEntity.java
@@ -4,10 +4,8 @@ import com.example.springboot.User.domain.type.Sex;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 @Getter // 접근자 필드에 접근하게 해준다 ? 필드에 Get 메소드 생성해줌
 @NoArgsConstructor // 매개변수가 없는 생성자
@@ -18,35 +16,40 @@ public class UserEntity {
     private Long id;
     // @Column = 속성 값을 정의(테이블), 필드를 테이블의 컬럼(열)에 매핑시킴
     // @Column 으로 작성해도 되지만 이렇게 작성해주는 이유는 자바에서도 인식하기 때문이다.
-    @NotNull
-    @Length(max = 255)
+    // 이렇게 작성 ? =     @NotNull ,  @Length(max = N)
+    @Column(length = 255, nullable = false)
     private String accountId;
 
-    @NotNull
-    @Length(max = 64)
+    @Column(length = 64, nullable = false)
     private String email;
 
-    @NotNull
-    @Length(max = 60)
+    @Column(length = 60, nullable = false)
     private String password;
 
-    @NotNull
-    @Length(max = 50)
+    @Column(length = 50, nullable = false)
     private String name;
 
-    @NotNull
-    @Length(max = 5)
+    @Column(length = 5, nullable = false)
     private Long studentId;
 
-    @NotNull
-    @Length(max = 6)
+    @Column(length = 6, nullable = false)
     private Sex sex;
+
     @Builder
     public UserEntity(String accountId, String email, String password, String name, Long studentId, Sex sex) {
         this.accountId = accountId;
         this.email = email;
         this.password = password;
         this.name = name;
+        this.studentId = studentId;
+        this.sex = sex;
+    }
+
+    public void update(String accountId, String password, String name, String email, Long studentId, Sex sex) {
+        this.accountId = accountId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
         this.studentId = studentId;
         this.sex = sex;
     }
