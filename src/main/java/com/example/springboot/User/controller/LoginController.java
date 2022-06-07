ce514aabe7676b468e8329c5d04f2fadf88ba8e8
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-06-07 22:03:08 +0900

    🌱 endpoint 수정

--- src/main/java/com/example/springboot/User/controller/LoginController.java
+++ src/main/java/com/example/springboot/User/controller/LoginController.java
@@ -5,15 +5,17 @@ import com.example.springboot.user.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/user")
 @RestController
 public class LoginController {
 
     private final LoginService loginService;
 
-    @PostMapping("/signin")
+    @PostMapping("/login")
     public void logIn(@RequestBody LoginRequest loginRequest) {
         loginService.logIn(loginRequest);
     }
